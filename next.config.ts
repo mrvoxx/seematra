7a2324a1abe591d8d76ced3d22e283cf657766b4
Developer Ashwin <developbyashwin@gmail.com>
2026-05-26 21:13:54 -0900

    fix: update CSP to allow leaflet map tiles and markers

--- next.config.ts
+++ next.config.ts
@@ -10,7 +10,7 @@ const CSP = [
   `script-src 'self' 'unsafe-inline' 'unsafe-eval' https://checkout.razorpay.com https://api.razorpay.com`,
   `style-src 'self' 'unsafe-inline' https://fonts.googleapis.com`,
   `font-src 'self' https://fonts.gstatic.com`,
-  `img-src 'self' data: blob: https://res.cloudinary.com https://lh3.googleusercontent.com https://images.unsplash.com`,
+  `img-src 'self' data: blob: https://res.cloudinary.com https://lh3.googleusercontent.com https://images.unsplash.com https://unpkg.com https://*.tile.openstreetmap.org`,
   `connect-src 'self' https://api.razorpay.com https://lumberjack.razorpay.com https://api.msg91.com`,
   `frame-src https://api.razorpay.com https://checkout.razorpay.com`,
   `object-src 'none'`,
